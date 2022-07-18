e1df51d2bf9d95c0d3d374af7441c0024f5be379
Peter <peter.pol.code@gmail.com>
2022-07-18 23:49:32 +0300

    sign token check middleware

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,16 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (post) => API.post("/posts", post);
 export const updatePost = (id, post) => API.patch(`/posts/${id}`, post);
